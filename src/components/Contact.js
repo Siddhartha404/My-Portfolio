fcf2515e58ad3fe183f15be716d8789916a6bb16
Siddhartha404 <sthapa-chhetri2@myseneca.ca>
2024-12-20 03:34:01 -0700

    added first project

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,8 +5,9 @@ function Contact() {
   return (
     <section id="contact">
       <h2>Contact Me</h2>
-      <p>Email: siddhartha@example.com</p>
+      <p>Email: tsiddhartha404@gmail.com </p>
       <p>LinkedIn: <a href="https://linkedin.com/in/siddhartha" target="_blank" rel="noopener noreferrer">linkedin.com/in/siddhartha</a></p>
+      <p>Github: <a href="https://github.com/Siddhartha404" target="_blank" rel="noopener noreferrer"> https://github.com/Siddhartha404 </a></p>
     </section>
   );
 }
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,11 +3,22 @@ import './Projects.css';
 
 function Projects() {
   return (
+    
     <section id="projects">
       <h2>Projects</h2>
       <div className="project-card">
-        <h3>Project 1</h3>
-        <p>Description of your first project.</p>
+        <a 
+          href="https://github.com/Siddhartha404/Network_scanner.git" 
+          target="_blank" 
+          rel="noopener noreferrer"
+        >
+          <h3>Network Scanner Tool</h3>
+          <p>
+            A Python-based tool for analyzing and monitoring local or remote networks. 
+            It identifies active devices within a specified network range, measures latency using ICMP pings, 
+            and scans for open ports. This tool leverages multithreading for optimal performance on larger networks.
+          </p>
+        </a>
       </div>
       <div className="project-card">
         <h3>Project 2</h3>
